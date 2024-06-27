bb5a622b10ac97ab5fd2aef6ad7d3cb2ff16f4a2
oozry12 <oozryr@gmail.com>
2024-06-27 09:58:29 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.ooim.xyz": dockerHub,
+  "quay.ooim.xyz": "https://quay.io",
+  "gcr.ooim.xyz": "https://gcr.io",
+  "k8s-gcr.ooim.xyz": "https://k8s.gcr.io",
+  "k8s.ooim.xyz": "https://registry.k8s.io",
+  "ghcr.ooim.xyz": "https://ghcr.io",
+  "cloudsmith.ooim.xyz": "https://docker.cloudsmith.io",
+  "ecr.ooim.xyz": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
